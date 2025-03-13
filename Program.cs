9233da29a84405ae6eed00499fb03f61b5faa8e8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 11:03:52 +0700

    Menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -1,33 +1,133 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class KodeProduk
-{
-    public enum ProdukElektronik
-    {
-        Laptop, Smartphone, Tablet, Headset, Keyboard,
-        Mouse, Printer, Monitor, Smartwatch, Kamera
-    };
+//class KodeProduk
+//{
+//    public enum ProdukElektronik
+//    {
+//        Laptop, Smartphone, Tablet, Headset, Keyboard,
+//        Mouse, Printer, Monitor, Smartwatch, Kamera
+//    };
 
-    public static string GetKodeProduk(ProdukElektronik produk)
-    {
-        string[] kodeProduk = {"E100", "E101", "E102", "E103", "E104",
-        "E105", "E106", "E107", "E108", "E109"};
+//    public static string GetKodeProduk(ProdukElektronik produk)
+//    {
+//        string[] kodeProduk = {"E100", "E101", "E102", "E103", "E104",
+//        "E105", "E106", "E107", "E108", "E109"};
 
-        return kodeProduk[(int)produk];
-    }
+//        return kodeProduk[(int)produk];
+//    }
     
+//}
+
+//public class Program
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Daftar Produk Elektronik dan Kode Produk: ");
+
+//        foreach(KodeProduk.ProdukElektronik produk in Enum.GetValues(typeof(KodeProduk.ProdukElektronik)))
+//        {
+//            string kodeProduk = KodeProduk.GetKodeProduk(produk);
+//            Console.WriteLine($"{produk} : {kodeProduk}");
+//        }
+//    }
+//}
+
+public class FanLaptop
+{
+    public enum State {Quite, Balanced, Performance, Turbo};
+
+    private State stateSekarang;
+
+    public FanLaptop()
+    {
+        stateSekarang = State.Quite;
+        Console.WriteLine("Quite Mode");
+    }
+
+    public void Perintah(string command)
+    {
+        switch (stateSekarang)
+        {
+            case State.Quite:
+                if (command == "Balanced")
+                {
+                    stateSekarang = State.Balanced;
+                    Console.WriteLine("Fan Quite berubah menjadi Balanced");
+                } else if (command == "Turbo")
+                {
+                    stateSekarang = State.Turbo;
+                    Console.WriteLine("Fan Quite berubah menjadi Turbo");
+                }
+                break;
+
+            case State.Balanced:
+                if (command == "Quite")
+                {
+                    stateSekarang = State.Quite;
+                    Console.WriteLine("Fan Balance berubah menjadi Quite");
+                } else if (command == "Performance")
+                {
+                    stateSekarang = State.Performance;
+                    Console.WriteLine("Fan Balance berubah menjadi Performance");
+                }
+                break;
+
+            case State.Turbo:
+                if (command == "Quite")
+                {
+                    stateSekarang = State.Quite;
+                    Console.WriteLine("Fan Turbo berubah menjadi Quite");
+                } else if (command == "Performance")
+                {
+                    stateSekarang = State.Performance;
+                    Console.WriteLine("Fan Turbo berubah menjadi Performance");
+                }
+                break;
+
+            case State.Performance:
+                if (command == "Balanced")
+                {
+                    stateSekarang = State.Balanced;
+                    Console.WriteLine("Fan Balanced berubah menjadi Balanced");
+                } else if (command == "Turbo")
+                {
+                    stateSekarang = State.Turbo;
+                    Console.WriteLine("Fan Balanced berubah menjadi Turbo");
+                }
+                break;
+        }
+    }
 }
 
 public class Program
 {
-    public static void Main()
+    static void Main()
     {
-        Console.WriteLine("Daftar Produk Elektronik dan Kode Produk: ");
+        FanLaptop kipas = new FanLaptop();
 
-        foreach(KodeProduk.ProdukElektronik produk in Enum.GetValues(typeof(KodeProduk.ProdukElektronik)))
-        {
-            string kodeProduk = KodeProduk.GetKodeProduk(produk);
-            Console.WriteLine($"{produk} : {kodeProduk}");
-        }
+        Console.WriteLine("\nQuite -> Balanced");
+        kipas.Perintah("Balanced");
+
+        Console.WriteLine("\nBalanced -> Performance");
+        kipas.Perintah("Performance");
+
+        Console.WriteLine("\nPerformance -> Turbo");
+        kipas.Perintah("Turbo");
+
+        Console.WriteLine("\nTurbo -> Quite");
+        kipas.Perintah("Quite");
+
+        Console.WriteLine("\nQuite -> Turbo");
+        kipas.Perintah("Turbo");
+
+        Console.WriteLine("\nTurbo -> Performance");
+        kipas.Perintah("Performance");
+
+        Console.WriteLine("\nPerformance -> Balanced");
+        kipas.Perintah("Balanced");
+
+        Console.WriteLine("\nBalanced -> Quite");
+        kipas.Perintah("Quite");
     }
 }
