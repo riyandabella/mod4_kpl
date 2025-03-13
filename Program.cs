5722346115f794c04658fe4600f9275834e2dc1e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:19:57 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class KodeProduk
+{
+    public enum ProdukElektronik
+    {
+        Laptop, Smartphone, Tablet, Headset, Keyboard,
+        Mouse, Printer, Monitor, Smartwatch, Kamera
+    };
+
+    public static string GetKodeProduk(ProdukElektronik produk)
+    {
+        string[] kodeProduk = {"E100", "E101", "E102", "E103", "E104",
+        "E105", "E106", "E107", "E108", "E109"};
+
+        return kodeProduk[(int)produk];
+    }
+    
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Daftar Produk Elektronik dan Kode Produk: ");
+
+        foreach(KodeProduk.ProdukElektronik produk in Enum.GetValues(typeof(KodeProduk.ProdukElektronik)))
+        {
+            string kodeProduk = KodeProduk.GetKodeProduk(produk);
+            Console.WriteLine($"{produk} : {kodeProduk}");
+        }
+    }
+}
